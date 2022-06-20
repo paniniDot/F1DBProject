c739d20e0933ef1cc73115883757ab3ec3e6633c
Mattia Panni <mattia.panni@studio.unibo.it>
2022-06-20 11:13:00 +0200

    Add Vettura

--- app/src/main/java/f1/model/Vettura.java
+++ app/src/main/java/f1/model/Vettura.java
@@ -1,5 +1,60 @@
 package f1.model;
 
-public class Vettura {
+import java.util.Objects;
 
+public class Vettura {
+	
+	private final String idVettura;
+	private final String nome;
+	private final int annoDiProduzione;
+	private final String idTeamProprietario;
+	
+	public Vettura(String idVettura, String nome, int annoDiProduzione, String idTeamProprietario) {
+		this.idVettura = idVettura;
+		this.nome = nome;
+		this.annoDiProduzione = annoDiProduzione;
+		this.idTeamProprietario = idTeamProprietario;
+	}
+
+	public String getIdVettura() {
+		return idVettura;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getAnnoDiProduzione() {
+		return annoDiProduzione;
+	}
+
+	public String getIdTeamProprietario() {
+		return idTeamProprietario;
+	}
+
+	@Override
+	public String toString() {
+		return "Vettura [idVettura=" + idVettura + ", nome=" + nome + ", annoDiProduzione=" + annoDiProduzione
+				+ ", idTeamProprietario=" + idTeamProprietario + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(annoDiProduzione, idTeamProprietario, idVettura, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vettura other = (Vettura) obj;
+		return annoDiProduzione == other.annoDiProduzione
+				&& Objects.equals(idTeamProprietario, other.idTeamProprietario)
+				&& Objects.equals(idVettura, other.idVettura) && Objects.equals(nome, other.nome);
+	}
+	
 }
